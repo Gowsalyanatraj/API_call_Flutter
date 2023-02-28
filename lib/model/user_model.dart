68d6fbfc20823c9b70b7c54fad8258aa5af97010
AgnRakul <rakul0agn@gmail.com>
2023-02-28 21:58:36 +0530

    fix: list render

---
+++ lib/model/user_model.dart
@@ -0,0 +1,37 @@
+// To parse this JSON data, do
+//
+//     final userModel = userModelFromJson(jsonString);
+
+import 'dart:convert';
+
+UserModel userModelFromJson(String str) => UserModel.fromJson(json.decode(str));
+
+String userModelToJson(UserModel data) => json.encode(data.toJson());
+
+class UserModel {
+    UserModel({
+        this.userId,
+        this.id,
+        this.title,
+        this.body,
+    });
+
+    int? userId;
+    int? id;
+    String? title;
+    String? body;
+
+    factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
+        userId: json["userId"],
+        id: json["id"],
+        title: json["title"],
+        body: json["body"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "userId": userId,
+        "id": id,
+        "title": title,
+        "body": body,
+    };
+}
